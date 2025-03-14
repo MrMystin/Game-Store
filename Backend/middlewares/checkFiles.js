bc7054584a8f55ecfd14e373c31ec80e74f57ec4
MrMystin <francisco.rocha@aluno.ifsp.edu.br>
2025-03-13 23:45:51 -0300

    Upgrading feat: check files

--- Backend/middlewares/checkFiles.js
+++ Backend/middlewares/checkFiles.js
@@ -1,24 +1,34 @@
-const checkFiles = (req, res, next) => {
-  const files = req.files;
-  const requiredFields = ['photos', 'banner', 'thumbnail', 'descriptionPhoto'];
+const fs = require('fs').promises
+const path = require('path')
+
+async function checkFiles(req, res, next) {
+  const requiredFields = ['photos', 'banner', 'descriptionPhoto'];
+  const minFiles = {photos: 5, banner: 1, descriptionPhoto: 2};
   var photoDetails = [];
-  let photoCounter = 1;
+
+  for (const field in minFiles) {
+    if (!req.files[field] || req.files[field].length < minFiles[field]) {
+      for (const field of Object.keys(req.files)) {
+        for (const file of req.files[field]) {
+          const filePath = path.join(path.dirname(__dirname), 'temp', `${file.filename}`);
+          await fs.rm(filePath, { recursive: true, force: true });
+        }
+      }
+      return next(Object.assign(new Error(`The minimum number of the following types of photos was not sent: ${field}`), { status: 400 }))
+    }
+  }
 
   for (const field of requiredFields) {
-    if (!files[field] || files[field].length === 0 || (field === 'photos' && files[field].length !== 5)) {
-      return res.sendStatus(400);
-    }
-
-    files[field].forEach(file => {
-      const extension = file.originalname.split('.').pop();
+    var counter = 1;
+    req.files[field].forEach(file => {
+      const extension = file.filename.split('.').pop();
       photoDetails.push({
-        photo: `${field}${field === 'photos' ? photoCounter++ : ''}.${extension}`,
+        photo: `${field}${req.files[field].length > 1 ? counter++ : ''}.${extension}`,
         type: field,
       });
     });
-    
-    req.photoDetails = photoDetails;
   }
+  req.body.photos = photoDetails;
 
   next()
 };
