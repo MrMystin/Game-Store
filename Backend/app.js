3121e4c8a63cdad81341ee56aee7c4d76ade6ece
MrMystin <francisco.rocha@aluno.ifsp.edu.br>
2025-03-13 23:43:39 -0300

    Feat: errorHandler

--- Backend/app.js
+++ Backend/app.js
@@ -7,8 +7,8 @@ var cors = require('cors');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
 var app = express();
+var errorHandler = require('./middlewares/errorHandler');
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -22,20 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(errorHandler)
 
 module.exports = app;
