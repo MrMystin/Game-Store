9c62e429f51d898f30519eb7a88af6d3fc0598e8
juhpurple <jussara.m@aluno.ifsp.edu.br>
2025-08-08 12:07:17 -0300

    iniciando login screen

--- Frontend-mobile/App.tsx
+++ Frontend-mobile/App.tsx
@@ -1,19 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import HomeScreen from './src/screens/HomeScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 export default function App() {
   return (
-    <HomeScreen />
+    <LoginScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+;
